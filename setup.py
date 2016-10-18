055806bf0c8f842c44c8c326bda32f224c92433e
Paul Renvoisé <renvoisepaul@gmail.com>
2016-10-18 15:03:49 +0200

    [REL] 2.2.3

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open(path.join(here, 'README.md'), 'r', 'utf-8') as f:
 
 setup(
     name="recastai",
-    version="2.2.2",
+    version="2.2.3",
     packages=find_packages(),
     description="Recast.AI official SDK for python",
     long_description=readme,
