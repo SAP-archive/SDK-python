a6016d5efbdb565e15ddc22f483a1126072116a5
Paul Renvoisé <renvoisepaul@gmail.com>
2017-03-20 15:11:21 +0100

    [FIX] prepend '/' to the connect endpoints

--- recastai/apis/connect/utils.py
+++ recastai/apis/connect/utils.py
@@ -3,5 +3,5 @@
 
 class Utils():
   """Endpoints"""
-  MESSAGE_ENDPOINT = 'https://api.recast.ai/connect/v1/messages'
-  CONVERSATION_ENDPOINT = 'https://api.recast.ai/connect/v1/conversations'
+  MESSAGE_ENDPOINT = 'https://api.recast.ai/connect/v1/messages/'
+  CONVERSATION_ENDPOINT = 'https://api.recast.ai/connect/v1/conversations/'
