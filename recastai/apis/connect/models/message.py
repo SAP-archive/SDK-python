8ba966ae536f748e5064572233ec1ba73b989a71
Paul Renvoisé <renvoisepaul@gmail.com>
2017-03-28 16:56:31 +0200

    [FIX] snake_case the response keys

--- recastai/apis/connect/models/message.py
+++ recastai/apis/connect/models/message.py
@@ -1,5 +1,6 @@
 # coding: utf-8
 
+import re
 import json
 import requests
 
@@ -11,7 +12,7 @@ class Message():
   def __init__(self, request):
     request = json.loads(request)
     for k, v in request.items():
-      setattr(self, k, v)
+      setattr(self, re.sub('(?!^)([A-Z])', r'_\1', k).lower(), v)
 
     self.conversation_id = request['message']['conversation']
     self.content = request['message']['attachment']['content']
