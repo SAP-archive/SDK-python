dc12623cc32fdca1fd24d92cb3a108c23a973a09
Paul Renvoisé <renvoisepaul@gmail.com>
2017-03-30 10:57:23 +0200

    [ADD] voice file converse

--- recastai/apis/request/converse.py
+++ recastai/apis/request/converse.py
@@ -35,3 +35,31 @@ class Converse():
       raise RecastError(response.json()['message'])
 
     return Conversation(response, token)
+
+  def converse_file(self, filename, token=None, language=None, conversation_token=None, memory=None):
+    token = token or self.token
+    if token is None:
+      raise RecastError("Token is missing")
+
+    language = language or self.language
+
+    filename = open(filename, 'rb') if not isinstance(filename, io.IOBase) else filename
+
+    # Build the body
+    body = {'voice': filename}
+    if language is not None:
+      body['language'] = language
+    if conversation_token is not None:
+      body['conversation_token'] = conversation_token
+    if memory is not None:
+      body['memory'] = memory
+
+    response = requests.post(
+      Utils.CONVERSE_ENDPOINT,
+      files=body,
+      headers={'Authorization': "Token {}".format(token)}
+    )
+    if response.status_code != requests.codes.ok:
+      raise RecastError(response.json()['message'])
+
+    return Conversation(response)
