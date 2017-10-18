af32126fbab776aaf54e1795f9fdfa39959d7c58
François Triquet <francois.triquet@recast.ai>
2017-10-18 22:09:41 +0200

    Export Build from module

--- recastai/__init__.py
+++ recastai/__init__.py
@@ -1,6 +1,6 @@
 # coding: utf-8
 
 from .client import Client
-from .apis import Connect, Request
+from .apis import Connect, Request, Build
 
-__all__ = ['Client', 'Connect', 'Request']
+__all__ = ['Client', 'Connect', 'Request', 'Build']
